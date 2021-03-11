826f6d0fbe50181786896210d70df48d3d1e0708
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2021-03-11 16:03:32 +0100

    adding squeeze excitation in cnn seld

--- keras_model.py
+++ keras_model.py
@@ -214,60 +214,7 @@ def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, f_pool_size, t_poo
 
     spec_cnn = Permute((2, 1, 3))(spec_cnn)
 
-    if is_tcn:
-        resblock_input = Reshape((data_in[-2], -1))(spec_cnn)
-
-        skip_connections = []
-
-        for d in range(10):
-
-            # 1D convolution
-            spec_conv1d = keras.layers.Convolution1D(filters=256,
-                                                     kernel_size=(3),
-                                                     padding='same',
-                                                     dilation_rate=2 ** d)(resblock_input)
-            spec_conv1d = BatchNormalization()(spec_conv1d)
-
-            # activations
-            tanh_out = keras.layers.Activation('tanh')(spec_conv1d)
-            sigm_out = keras.layers.Activation('sigmoid')(spec_conv1d)
-            spec_act = keras.layers.Multiply()([tanh_out, sigm_out])
-
-            # spatial dropout
-            spec_drop = keras.layers.SpatialDropout1D(rate=0.5)(spec_act)
-
-            # 1D convolution
-            skip_output = keras.layers.Convolution1D(filters=128,
-                                                     kernel_size=(1),
-                                                     padding='same')(spec_drop)
-
-            res_output = keras.layers.Add()([resblock_input, skip_output])
-
-            if skip_output is not None:
-                skip_connections.append(skip_output)
-
-            resblock_input = res_output
-        # ---------------------------------------
-
-        # Residual blocks sum
-        spec_sum = keras.layers.Add()(skip_connections)
-        spec_sum = keras.layers.Activation('relu')(spec_sum)
-
-        # 1D convolution
-        spec_conv1d_2 = keras.layers.Convolution1D(filters=128,
-                                                   kernel_size=(1),
-                                                   padding='same')(spec_sum)
-        spec_conv1d_2 = keras.layers.Activation('relu')(spec_conv1d_2)
-
-        # 1D convolution
-        spec_tcn = keras.layers.Convolution1D(filters=128,
-                                              kernel_size=(1),
-                                              padding='same')(spec_conv1d_2)
-        spec_tcn = keras.layers.Activation('tanh')(spec_tcn)
-
-        spec_cnn = spec_tcn
-
-    # RNN    
+    # RNN
     spec_rnn = Reshape((data_out[-2] if is_accdoa else data_out[0][-2], -1))(spec_cnn)
     for nb_rnn_filt in rnn_size:
         spec_rnn = Bidirectional(
@@ -312,15 +259,120 @@ def get_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, f_pool_size, t_poo
     return model
 
 
+def get_seldtcn_model(data_in, data_out, dropout_rate, nb_cnn2d_filt, pool_size, fnn_size, weights, ratio):
+    # model definition
+    spec_start = Input(shape=(data_in[-3], data_in[-2], data_in[-1]))
+    spec_cnn = spec_start
+
+    # CONVOLUTIONAL LAYERS =========================================================
+    for i, convCnt in enumerate(pool_size):
+        spec_aux = spec_cnn
+        spec_cnn = Conv2D(nb_cnn2d_filt, 3, padding='same')(spec_cnn)
+        spec_cnn = BatchNormalization()(spec_cnn)
+        spec_cnn = ELU()(spec_cnn)
+        spec_cnn = Conv2D(nb_cnn2d_filt, 3, padding='same')(spec_cnn)
+        spec_cnn = BatchNormalization()(spec_cnn)
+
+        spec_aux = Conv2D(nb_cnn2d_filt, 1, padding='same')(spec_aux)
+        spec_aux = BatchNormalization()(spec_aux)
+
+        spec_cnn = add([spec_cnn, spec_aux])
+        spec_cnn = ELU()(spec_cnn)
+
+        spec_cnn = channel_spatial_squeeze_excite(spec_cnn, ratio=ratio)
+
+        spec_cnn = add([spec_cnn, spec_aux])
+        spec_cnn = MaxPooling2D(pool_size=(1, pool_size[i]))(spec_cnn)
+        spec_cnn = Dropout(dropout_rate)(spec_cnn)
+    spec_cnn = Permute((2, 1, 3))(spec_cnn)
+    resblock_input = Reshape((data_in[-2], -1))(spec_cnn)
+
+    # TCN layer ===================================================================
+
+    # residual blocks ------------------------
+    skip_connections = []
+
+    for d in range(10):
+
+        # 1D convolution
+        spec_conv1d = keras.layers.Convolution1D(filters=256,
+                                                 kernel_size=(3),
+                                                 padding='same',
+                                                 dilation_rate=2 ** d)(resblock_input)
+        spec_conv1d = BatchNormalization()(spec_conv1d)
+
+        # activations
+        tanh_out = keras.layers.Activation('tanh')(spec_conv1d)
+        sigm_out = keras.layers.Activation('sigmoid')(spec_conv1d)
+        spec_act = keras.layers.Multiply()([tanh_out, sigm_out])
+
+        # spatial dropout
+        spec_drop = keras.layers.SpatialDropout1D(rate=0.5)(spec_act)
+
+        # 1D convolution
+        skip_output = keras.layers.Convolution1D(filters=128,
+                                                 kernel_size=(1),
+                                                 padding='same')(spec_drop)
+
+        res_output = keras.layers.Add()([resblock_input, skip_output])
+
+        if skip_output is not None:
+            skip_connections.append(skip_output)
+
+        resblock_input = res_output
+    # ---------------------------------------
+
+    # Residual blocks sum
+    spec_sum = keras.layers.Add()(skip_connections)
+    spec_sum = keras.layers.Activation('relu')(spec_sum)
+
+    # 1D convolution
+    spec_conv1d_2 = keras.layers.Convolution1D(filters=128,
+                                               kernel_size=(1),
+                                               padding='same')(spec_sum)
+    spec_conv1d_2 = keras.layers.Activation('relu')(spec_conv1d_2)
+
+    # 1D convolution
+    spec_tcn = keras.layers.Convolution1D(filters=128,
+                                          kernel_size=(1),
+                                          padding='same')(spec_conv1d_2)
+    spec_tcn = keras.layers.Activation('tanh')(spec_tcn)
+
+    # SED ==================================================================
+    sed = spec_tcn
+    for nb_fnn_filt in fnn_size:
+        sed = TimeDistributed(Dense(nb_fnn_filt))(sed)
+        sed = Dropout(dropout_rate)(sed)
+    sed = TimeDistributed(Dense(data_out[0][-1]))(sed)
+    sed = Activation('sigmoid', name='sed_out')(sed)
+
+    # DOA ==================================================================
+    doa = spec_tcn
+    for nb_fnn_filt in fnn_size:
+        doa = TimeDistributed(Dense(nb_fnn_filt))(doa)
+        doa = Dropout(dropout_rate)(doa)
+
+    doa = TimeDistributed(Dense(data_out[1][-1]))(doa)
+    doa = Activation('tanh', name='doa_out')(doa)
+
+    model = Model(inputs=spec_start, outputs=[sed, doa])
+    model.compile(optimizer=Adam(), loss=['binary_crossentropy', 'mse'], loss_weights=weights)
+
+    model.summary()
+    return model
+
+
 def masked_mse(y_gt, model_out):
-    nb_classes = 12 #TODO fix this hardcoded value of number of classes
+    nb_classes = 12  # TODO fix this hardcoded value of number of classes
     # SED mask: Use only the predicted DOAs when gt SED > 0.5
-    sed_out = y_gt[:, :, :nb_classes] >= 0.5 
+    sed_out = y_gt[:, :, :nb_classes] >= 0.5
     sed_out = keras.backend.repeat_elements(sed_out, 3, -1)
     sed_out = keras.backend.cast(sed_out, 'float32')
 
     # Use the mask to computed mse now. Normalize with the mask weights 
-    return keras.backend.sqrt(keras.backend.sum(keras.backend.square(y_gt[:, :, nb_classes:] - model_out[:, :, nb_classes:]) * sed_out))/keras.backend.sum(sed_out)
+    return keras.backend.sqrt(keras.backend.sum(
+        keras.backend.square(y_gt[:, :, nb_classes:] - model_out[:, :, nb_classes:]) * sed_out)) / keras.backend.sum(
+        sed_out)
 
 
 def load_seld_model(model_file, doa_objective):
@@ -331,6 +383,3 @@ def load_seld_model(model_file, doa_objective):
     else:
         print('ERROR: Unknown doa objective: {}'.format(doa_objective))
         exit()
-
-
-
