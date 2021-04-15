e70555b2992e5e95e76dccd6f188716085d7a3d9
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2021-04-15 21:27:41 +0200

    adding utils for concatenation

--- parameter.py
+++ parameter.py
@@ -14,13 +14,13 @@ def get_params(argv='1'):
         dataset_dir='base_folder/',  # Base folder containing the foa_dev/mic_dev and metadata folders
 
         # OUTPUT PATH
-        feat_label_dir='seld_feat_label_foa/',  # Directory to dump extracted features and labels
+        feat_label_dir='seld_feat_label_both/',  # Directory to dump extracted features and labels
         model_dir='models/',  # Dumps the trained models and training curves in this folder
         dcase_output_dir='results/',  # recording-wise results are dumped in this path.
 
         # DATASET LOADING PARAMETERS
         mode='dev',  # 'dev' - development or 'eval' - evaluation dataset
-        dataset='foa',  # 'foa' - ambisonic or 'mic' - microphone signals
+        dataset='both',  # 'foa' - ambisonic or 'mic' - microphone signals
 
         # FEATURE PARAMS
         fs=24000,
@@ -39,7 +39,7 @@ def get_params(argv='1'):
 
         is_baseline=False,
         ratio=1,
-        is_tcn=True,
+        is_tcn=False,
 
         nb_cnn2d_filt=64,  # Number of CNN nodes, constant for each layer
         f_pool_size=[4, 4, 2],
